cae7196d87796d2672f954fd6b3f07820a96eae4
SUZUKIyuka <suzukiyuka1218@gmail.com>
2015-02-14 23:42:20 +0900

    update README

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # first_app3
+by railstutorial.jp
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp3::Application.config.secret_token = 'd913817284255046f7d481326b7b2f556abd615a97044f1806098985fa2a9b7853e8fc5c1677d361adea5ac1833601b0834e0d950e58f0b7b59fd0c7c3831a5b'
